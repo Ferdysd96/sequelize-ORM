6c9999082a889d7b1184ebb54a27219f2770f1f1
Ferdys Duran <ferdysd96@gmail.com>
2020-06-08 22:50:13 -0400

    arreglos

--- src/app.js
+++ src/app.js
@@ -16,7 +16,6 @@ app.use(compression());
 app.use(express.json());
 app.use(cors());
 
-
 //Routes
 app.get('/', (req, res) => res.send('<h1>Welcome</h1>'));
 app.use('/session', require('./routes/session'));
